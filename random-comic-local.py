97685890ccb8bec2b46627244580613d80d3774d
Duedot43 <jackgendill@icloud.com>
2022-04-25 08:50:53 -0600

    local

---
+++ random-comic-local.py
@@ -0,0 +1,10 @@
+def update_xkcd(folder_location):
+    pass
+def random_xkcd(folder_location):
+    pass
+def update_gar(folder_location):
+    pass
+def random_gar(folder_location):
+    pass
+def random_all(xkcd_folder, gar_folder):
+    pass
