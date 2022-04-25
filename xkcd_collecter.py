b1d84e4416ba3f14741164d170c99c1056c69e89
Duedot43 <jackgendill@icloud.com>
2022-04-24 23:46:20 -0600

    xkcd :D

--- xkcd_collecter.py
+++ xkcd_collecter.py
@@ -7,6 +7,18 @@ def getval(file):
         p2 = str(res.text)
         return p2
 os.makedirs("xkcd_comics")
-get = getval("1")
-jsont = json.loads(get)
-print(jsont['img'])
+res = requests.get("http://xkcd.com/info.0.json")
+if res.status_code == 200:
+    p2 = str(res.text)
+jsont = json.loads(p2)
+img_list = []
+
+for x in range(1,jsont['num']):
+    if x != 404:
+        get = getval(str(x))
+        jsont = json.loads(get)
+        img_list.append(jsont['num'])
+        os.system(f"curl '{jsont['img']}' --output ./xkcd_comics/{jsont['num']}.png")
+
+with open('./xkcd_comics/latest', 'a') as f:
+    f.write(str(img_list))
